24495c22a0705c1e2333a49267f22cd196c4a82e
Gabriela <gaby.rivero.admi1@gmail.com>
2024-06-12 18:53:05 -0300

    cart ok

--- src/routes/routes.cart.js
+++ src/routes/routes.cart.js
@@ -103,10 +103,14 @@ router.post("/", (req, res) => {
   const nuevoCarrito = req.body;
 
   try {
-      cart.push(nuevoCarrito);
-      res.status(201).send({ status: "success", message: "Carrito creado exitosamente" });
+    const lastId = cart.length > 0 ? cart[cart.length - 1].id : 0;
+    nuevoCarrito.id = lastId + 1;
+
+    cart.push(nuevoCarrito);
+    res.status(201).send({ message: "Carrito creado exitosamente" });
+
   } catch (error) {
-      res.status(500).send({ status: "error", message: "Error al crear el carrito" });
+    res.status(500).send({ message: "Error al crear el carrito" });
   }
 });
   
@@ -127,6 +131,33 @@ router.get("/:pid", (req, res) => {
 //Agregar un producto y cantidad al carrito seleccionado:
 
 
+router.post("/:cid/product/:pid", (req, res) => {
+  const cartId = parseInt(req.params.cid);
+  const productId = req.params.pid;
+  const quantity = req.body.quantity || 1;
+
+  try {
+    const cartFound = cart.find(c => c.id === cartId);
+
+    if (!cartFound) {
+      return res.status(404).json({ error: "Carrito no encontrado" });
+    }
+
+    const productIndex = cartFound.products.findIndex(p => p.product === productId);
+
+    if (productIndex !== -1) {
+      
+      cartFound.products[productIndex].quantity += quantity;
+    } else {
+    
+      cartFound.products.push({ product: productId, quantity: quantity });
+    }
+
+    res.json(cartFound.products);
+  } catch (error) {
+    res.status(500).send({ status: "error", message: "Error al agregar producto al carrito" });
+  }
+});
 
 
 
